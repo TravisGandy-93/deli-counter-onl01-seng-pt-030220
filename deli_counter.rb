df278c9bfab65929bd9696ea0c854c88adb05ee4
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 05:26:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ puts "Welcome, #{newbie}. You are number #{spot} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli.size = 0 
+  if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
   else 
      puts "Currently serving #{katz_deli[0]}."
