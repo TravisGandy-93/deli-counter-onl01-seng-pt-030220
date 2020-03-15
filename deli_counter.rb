69dc6b10aac28ecf3314b0831f243ff2734d8100
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 05:06:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  string = "The line is currently"
+if katz_deli.size == 0
+ puts string + " empty." 
+ else 
+i=1 
+waiting = []
+katz_deli.each do |name|
+      waiting << "#{i}. #{name}"
+    i += 1
+ end
+ puts string + ": " + waiting.join(" ")
+ end
+end
+
+def take_a_number(katz_deli,newbie)
+katz_deli.push(newbie)
+end
