f1905b27d507a3096046819175feb46a90fc3adf
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 05:24:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size = 0 
-    puts 
+    puts "There is nobody waiting to be served!
   puts "Currently serving #{katz_deli[0]}."
   katz_deli.shift
   
