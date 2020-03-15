2564fbfb31eb972160202ebe98b14fb398d5e422
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 05:25:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,8 +23,9 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size = 0 
-    puts "There is nobody waiting to be served!
-  puts "Currently serving #{katz_deli[0]}."
+    puts "There is nobody waiting to be served!"
+  else 
+     puts "Currently serving #{katz_deli[0]}."
   katz_deli.shift
   
 end
